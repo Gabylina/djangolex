55bca1e2e60f462b1877092f7eb7265cbccd62fa
Seba <se.bernal@duocuc.cl>
2023-06-12 16:19:02 -0400

    cambios url

--- servidor/aplicacion/urls.py
+++ servidor/aplicacion/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index,iniciosesion,crearcuenta,solicitudes,PAGINAGABY,solicitudestecjuri,indextecjuri, presupuestostecjuri
+from .views import index,iniciosesion,crearcuenta,solicitudes,PAGINAGABY,solicitudestecjuri,indextecjuri, presupuestostecjuri,pago
 
 
 
